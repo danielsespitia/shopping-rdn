ee566af7c7413b82a1963fa8b448e35212d9c6b2
Daniel Espitia <dsespitiac@gmail.com>
2021-07-01 19:31:12 -0500

    added styles to collection item and collection preview

---
+++ src/components/atoms/CollectionItem/index.jsx
@@ -0,0 +1,17 @@
+// Packages
+import React from 'react';
+
+// Styles
+import './index.styles.scss';
+
+export const CollectionItem = ({ name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+    </div>
+  );
+};
--- src/components/molecules/CollectionPreview/index.jsx
+++ src/components/molecules/CollectionPreview/index.jsx
@@ -1,4 +1,8 @@
+// Packages
 import React from 'react';
+// Components
+import { CollectionItem } from '../../atoms';
+// Styles
 import './index.styles.scss';
 
 export const CollectionPreview = ({ title, items }) => {
@@ -8,9 +12,12 @@ export const CollectionPreview = ({ title, items }) => {
     <div className="collection-preview">
       <h1 className="title">{upperCaseTitle}</h1>
       <div className="preview">
-        {items.map((item) => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        {items
+          // Performance issues (gets re-rendered on every call)
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...collectionItemProps }) => (
+            <CollectionItem key={id} {...collectionItemProps} />
+          ))}
       </div>
     </div>
   );
