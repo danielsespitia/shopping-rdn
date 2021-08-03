a71f6f0a96f7f863dbab879f85dc50b50144f2cc
Daniel Espitia <dsespitiac@gmail.com>
2021-08-03 18:55:03 -0500

    added persistance with session storage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+// Packages
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
 // Styles
 import './index.css';
 // App
 import App from './App';
 // Store
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 // Packages
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 // Reducer
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
