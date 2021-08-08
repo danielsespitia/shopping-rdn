8075eb768ed3ba37ee64bd5d7bf73a4955150a63
Daniel Espitia <dsespitiac@gmail.com>
2021-08-08 18:39:21 -0500

    created selector to convert object to array

--- src/components/molecules/CollectionsOverview/index.jsx
+++ src/components/molecules/CollectionsOverview/index.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 // Selectors
-import { selectCollections } from '../../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../../redux/shop/shop.selectors';
 // Components
 import { CollectionPreview } from '../../';
+// Styles
+import './index.styles.scss';
 
 export const CollectionsOverview = ({ collections }) => {
   return (
-    <div>
+    <div className="collections-overview">
       {!!collections &&
         collections.map(({ id, ...collectionPreviewProps }) => (
           <CollectionPreview key={id} {...collectionPreviewProps} />
@@ -19,7 +21,7 @@ export const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export const ConnectedCollectionsOverview = connect(mapStateToProps)(
